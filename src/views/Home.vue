e97b0b375c2ffee16105415c476d35c2b018889c
wanghong <34284143@qq.com>
2019-08-11 11:13:29 +0800

    修改首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <p>{{ value }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
+  components: {},
   data() {
     return {
-      value: "123"
+      value: "首页"
     };
   }
 };
