7e93ff692f0450247d8924e05a7011d25b54ca87
wanghong <34284143@qq.com>
2019-08-11 20:50:26 +0800

    store,vuex测试数据

---
+++ src/store/getters.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+const getters = {
+  // device: state => state.app.device,
+  // theme: state => state.app.theme,
+  // color: state => state.app.color,
+  // token: state => state.user.token,
+};
+
+export default getters;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import app from "./modules/app";
+import user from "./modules/user";
+
 Vue.use(Vuex);
 
 //定义store 整个项目就这一个
 export default new Vuex.Store({
+  modules: {
+    app,
+    user
+  },
   state: {},
   mutations: {},
   actions: {}
