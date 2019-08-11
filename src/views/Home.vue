354ca086d0f25d76de14eab30068153629d27d07
wanghong <34284143@qq.com>
2019-08-11 10:48:25 +0800

    初始化配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ value }}</p>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      value: "123"
+    };
   }
 };
 </script>
