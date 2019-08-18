4b9419fd4299cd191499726e6e1568b46c21147f
wanghong <34284143@qq.com>
2019-08-18 17:36:19 +0800

    vuejsx

---
+++ src/components/TestJsx.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h2>vue jsx</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestJsx"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/jsx/JsxDemo1.vue
+++ src/views/jsx/JsxDemo1.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        jsxdemo01
-    </div>
+  <div>
+    <TestJsx />
+  </div>
 </template>
 
 <script>
-    export default {
-        name:"jsxdemo1"
-    }
+import TestJsx from "../../components/TestJsx";
+export default {
+  name: "jsxdemo1",
+  components: {
+    TestJsx
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
